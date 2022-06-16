2f63b31a265d886c7e459fabdeadfe6a79acf7a7
Malcolm McJimsey <mmcjimsey26@outlook.com>
2022-06-16 14:26:43 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+<div align="center">
+
+# yuck-mode
+
+Emacs major mode for editing yuck configuration files for [ElKowar's Wacky Widgets (eww)](https://github.com/elkowar/eww). I'll answer your burning questions below.
+
+</div>
+
+## Note:
+This package is not yet available on MELPA, although it will be in the near future.
+
+## What even is this?
+
+`yuck-mode` is a barebones (65 sloc) major mode for editing yuck config files. It is nothing more than the most minimal major mode that you can think of.
+`yuck-mode` only includes syntax highlighting functionality. Everything else is inherited through Emacs' `prog-mode`. `yuck-mode` is not Flycheck, so don't expect linting. I may add this feature in the future, but it does seem a bit overkill.
+
+## Why?
+
+Large and convoluted configuration files often obscure exactly what's going on and can introduce problems that can be solved
+even with simple syntax highlighting. It may not be for everyone, but it can be useful at times.
+
+## What features are there?
+
++ Syntax highlighting for all keywords and widget types in the yuck configuration language.
+
+That's literally everything.
+
+## How do I install it?
+
+yuck-mode is available on MELPA (TODO).
+If you'd like to do it the hard way and install it manually,
+just add yuck-mode.el to your `load-path` and add the following to your ~/.emacs (or if Doom Emacs, your ~/.doom.d/config.el):
+```lisp
+(autoload 'yuck-mode "yuck-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.yuck\\'" . yuck-mode))
+```
+
+## How do I contribute?
+
+If you want to contribute anything, from fixing a bug to adding new 
+features, feel free to fork the repo and open a pull request as soon as you're finished.
---
+++ yuck-mode.el
@@ -0,0 +1,82 @@
+;;; yuck-mode.el --- Major mode for the yuck configuration language -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 mmcjimsey26
+;;
+;; Author: mmcjimsey26
+;; Maintainer: mmcjimsey26
+;; Created: June 14, 2022
+;; Modified: June 14, 2022
+;; Version: 0.0.1
+;; Keywords: languages yuck eww widgets
+;; Homepage: https://github.com/mmcjimsey26/yuck-mode
+;; Package-Requires: ((emacs "25.1"))
+;;
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; A simple major mode for editing files in the yuck configuration language, used for
+;; configuring ElKowar's Wacky Widgets (eww), usually ending in `.yuck'.
+
+;; This package provides the following features:
+;;      * Syntax hilighting
+
+;;; Installation:
+
+;; This mode requires Emacs-25.1 or higher.
+;; Put this file into `load-path' and the following into ~/.emacs
+;;      (autoload 'yuck-mode "yuck-mode" nil t)
+;;      (add-to-list 'auto-mode-alist '("\\.yuck\\'" . yuck-mode))
+
+;;; Code:
+
+(defgroup yuck nil
+  "Yuck Configuration Major Mode."
+  :group 'languages)
+
+(defconst yuck-mode-menu nil "Menu for yuck mode.")
+
+(defvar yuck-mode-hook nil "Yuck mode hook.")
+
+;; Keywords and widget types for `yuck-mode'.
+(defvar yuck-keywords-list
+  '("defvar" "defpoll" "deflisten" "defwindow" "defwidget" "for" "include"))
+
+(defvar yuck-widgets-list
+  '("combo-box-text"
+    "expander" "revealer" "checkbox" "color-button" "color-chooser" "scale" "progress"
+    "input" "button" "image" "box" "overlay" "centerbox" "scroll" "eventbox"
+    "label" "literal" "calendar" "transform" "circular-progress" "graph"))
+
+(defvar yuck-keywords-regex)
+(defvar yuck-widgets-regex)
+(defvar yuck-font-lock-keywords)
+
+;; Regex to highlight buffer.
+(setq yuck-keywords-regex (regexp-opt yuck-keywords-list 'words))
+(setq yuck-widgets-regex (regexp-opt yuck-widgets-list 'words))
+
+(setq yuck-font-lock-keywords
+  `(
+    (,yuck-keywords-regex . font-lock-keyword-face)
+    (,yuck-widgets-regex . font-lock-type-face)
+    (,"\:[a-z\-]*" . font-lock-function-name-face)))
+
+(defconst yuck-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    ;; "" : string delimiter
+    (modify-syntax-entry ?\" "\"" table)
+    (modify-syntax-entry ?\` "\"" table)
+    (modify-syntax-entry ?\; "<" table)
+    (modify-syntax-entry ?\n ">" table)
+    table))
+
+;;;###autoload
+(define-derived-mode yuck-mode prog-mode "Yuck"
+  "Major mode for editing yuck config files."
+  :syntax-table yuck-mode-syntax-table
+  (setq font-lock-defaults '(yuck-font-lock-keywords))
+  (font-lock-ensure))
+
+(provide 'yuck-mode)
+;;; yuck-mode.el ends here
